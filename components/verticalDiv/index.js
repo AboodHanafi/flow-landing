3a7d8e027c910ea1a5f025860682d2f1e35fd755
AboodHanafi <aboodhanafi10@gmail.com>
2024-05-26 16:18:15 +0300

    change hero Pictures

--- components/verticalDiv/index.js
+++ components/verticalDiv/index.js
@@ -2,11 +2,14 @@ import React from "react";
 
 function Feature({ icon, feature, featureInfo }) {
   return (
-    <div className="w-[80%] bg-cyan-700 m-auto grid grid-cols-7 divide-x-2 gap-4 text-xl">
-      <span className="px-6 py-8 border rounded-full">{icon}</span>
-
-      <p className="col-span-2 px-6 py-8">{feature}</p>
-      <p className="col-span-4 px-6 py-8">{featureInfo}</p>
+    <div className="relative shadow m-auto grid md:grid-cols-7 grid-cols-1 items-center gap-4 ">
+      <span className="px-6 py-8 rounded-full ">{icon}</span>
+      <p className="md:col-span-2 px-6 py-2 md:py-8 md:border-l-4 text-green-500 font-bold text-xl md:border-r-4  md:border-l-green-500 md:border-r-green-500">
+        {feature}
+      </p>
+      <p className="md:col-span-4 px-6 py-2 md:py-8 text-green-500 text-xl">
+        {featureInfo}
+      </p>
     </div>
   );
 }
